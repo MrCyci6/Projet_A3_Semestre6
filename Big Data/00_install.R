79e97c1dc1196f7b690a0aaaafe631309bc2a814
MrCyci6 <nozuriafaction@gmail.com>
2026-06-08 16:11:04 +0200

    feat: Installation script

---
+++ Big Data/00_install.R
@@ -0,0 +1,6 @@
+install.packages(c(
+  "data.table", "ggplot2", "dplyr",   # manip + graphiques
+  "leaflet", "sf",                     # cartes
+  "corrplot", "GGally",                # corrélations
+  "caret", "randomForest"              # modèles / régression
+))
