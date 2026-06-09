254bd395af02ee5750e83882dd10a47f7eea21a6
Mgx <margaux.caradant@isen-ouest.yncrea.fr>
2026-06-09 16:49:58 +0200

    Carte et visu update
    Carte et visu update

--- Big Data/02_visualisation.R
+++ Big Data/02_visualisation.R
@@ -8,46 +8,45 @@ head(data$date_mise_en_service)
 #tri des données dont on a besoin partie 1 
 
 #mise en format et bornes temporelles (vérifié par ia pour les tirets au lieu de /)
-    date_debut <- as.POSIXct("01-01-2021 00:00", format = "%d-%m-%Y %H:%M",)
-    date_fin <- as.POSIXct("01-01-2027 00:00", format = "%d-%m-%Y %H:%M")
-    data$date_mise_en_service <- as.POSIXct(data$date_mise_en_service, 
+date_debut <- as.POSIXct("01-01-2021 00:00", format = "%d-%m-%Y %H:%M",)
+date_fin <- as.POSIXct("01-01-2027 00:00", format = "%d-%m-%Y %H:%M")
+data$date_mise_en_service <- as.POSIXct(data$date_mise_en_service, 
                                 format = "%d-%m-%Y %H:%M")
 
     #récupération des données entre les bornes 
-    data_analyse <- data[!is.na(data$date_mise_en_service) & 
+ data_analyse <- data[!is.na(data$date_mise_en_service) & 
                         data$date_mise_en_service >= date_debut &
                         data$date_mise_en_service <= date_fin,]
 
     #création d'une nouvelle colonne, puis d'une table avec toutes les données
-    data_analyse$trimestre <- paste0(format(data_analyse$date_mise_en_service, "%Y"),
+data_analyse$trimestre <- paste0(format(data_analyse$date_mise_en_service, "%Y"),
                                     "-",
                                     quarters(data_analyse$date_mise_en_service))
 
     # 3. Création de la table par trimestre
-    evolution_trimestre <- table(data_analyse$trimestre)
-    evolution_tot <- as.data.frame(evolution_trimestre)
+evolution_trimestre <- table(data_analyse$trimestre)
+evolution_tot <- as.data.frame(evolution_trimestre)
 
-    par(mar = c(5, 7, 4, 2))
 
     #visualisation graphique
-    positions <- barplot(evolution_tot$Freq,
-        space = 0,
-        main="Evolution du nombre de stations mises en service par année",
-        xlab = "Temps (année)",
-        ylab= "Nombre de station",
-        ylim = c(0,6000),
-        las =1)
+positions <- barplot(evolution_tot$Freq,
+    space = 0,
+    main="Evolution du nombre de stations mises en service par année",
+    xlab = "Temps (année)",
+    ylab= "Nombre de station",
+    ylim = c(0,5000),
+    las =1)
 
-    un_sur_deux <- seq(1, nrow(evolution_tot), by = 2)
+un_sur_deux <- seq(1, nrow(evolution_tot), by = 2)
 
-    positions_centrees <- un_sur_deux - 0.5
+positions_centrees <- un_sur_deux - 0.5
 
-    axis(1, 
-        at = positions_centrees,                  
-        labels = evolution_tot$Var1[un_sur_deux],           
-        las = 2,                                           
-        adj = 1,
-        cex.axis = 0.8)
+axis(1, 
+    at = positions_centrees,
+    labels = evolution_tot$Var1[un_sur_deux],
+    las = 2,
+    adj = 1,
+    cex.axis = 0.8)
 
 #tri des données étape 2 histogrammes
 #puissance nominale
@@ -66,7 +65,7 @@ barplot(repartition_puissance,
     las = 1,
     main ="Répartition des puissances en kw",
     xlab = "Nombre de stations",
-    xlim =c(0,30000),
+    xlim =c(0,25000),
     ylab = "Puissance")
 
 #type de prise
@@ -91,5 +90,5 @@ barplot(comptage_prise,
     las = 1,
     main ="Répartition des prises installées",
     xlab = "Nombre de station concernées",
-    xlim =c(0,50000))
+    xlim =c(0,40000))
 
--- Big Data/03_carte.R
+++ Big Data/03_carte.R
@@ -1,1 +1,39 @@
+#options(timeout = 300)
+#install.packages("leaflet", dependencies = TRUE)
+#install.packages("leaflet.extras")
+
+library(leaflet)
+library(maps)
+library(leaflet.extras)
+
+#on récupère les données géographiques
 data <- fread("./data/IRVE_clean.csv")
+data_geo <- data_analyse[, c("latitude", "longitude")]
+data_geo <- data_analyse[!is.na(consolidated_latitude) & !is.na(consolidated_longitude), 
+                         .(consolidated_longitude, consolidated_latitude)]
+
+#
+data_analyse$categorie_puissance <- cut(data_analyse$puissance_nominale,
+                                    breaks = c(0, 3.7, 7.4, 11, 22, 43, 50, 150, Inf),
+                                    labels = c("3.7kW", "7.4kW", "11kW", "22kW", "43kW",
+                                     "50kW", "50 à \n 150kW", ">150kW"))
+
+categories <- c("3.7kW", "7.4kW", "11kW", "22kW", "43kW", "50kW", "50 à 150kW", ">150kW")
+categories_ordonnees <- factor(categories, levels = les_categories)
+
+palette_globale <- colorFactor( palette = c("#33e923", "#21a008", "#0d7909", "#086d6d", "#d12497", "#a51a70", "#4f0b55", "#1e062c"),
+                                domain = categories_ordonnees)
+# Création de la carte interactive 
+carte_base <- leaflet(data_geo)
+carte_fonds <- addTiles(carte_base)
+carte_cadree <- setView(carte_fonds, lng = 2.2137, lat = 46.2276, zoom = 6)
+carte_chaleur <- addHeatmap(carte_cadree, lng = ~consolidated_longitude, lat = ~consolidated_latitude, blur = 20, max = 0.05, radius = 15)
+carte_marqueur <- addMarkers(carte_chaleur, lng = ~consolidated_longitude, lat = ~consolidated_latitude, clusterOptions = markerClusterOptions())
+
+#Ajout d'une partie supplémentaire sur les bornes de puissances + la légende (avec l'aide de l'ia)
+carte_puissance <- addCircleMarkers(carte_marqueur, lng = ~consolidated_longitude, lat = ~consolidated_latitude,
+                                radius = 7,color = ~palette_globale(categories_puissance),
+                                clusterOptions = markerClusterOptions())
+carte_finale <- addLegend(carte_puissance, pal = palette_globale, values = categories_ordonnees,
+                        position = "bottomright", title = "Type de recharge")
+carte_finale
