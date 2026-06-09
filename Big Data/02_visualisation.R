313e3148727e557100d466f212efbd190d794b41
Mgx <margaux.caradant@isen-ouest.yncrea.fr>
2026-06-09 10:58:32 +0200

    Update 02_visualisation.R
    Courbe + diagramme

--- Big Data/02_visualisation.R
+++ Big Data/02_visualisation.R
@@ -23,9 +23,38 @@ data_analyse$annee_mois <- format(data_analyse$date_mise_en_service, "%Y-%m")
 evolution_stations <- table(data_analyse$annee_mois)
 #mise en place d'une data frame pour la visualisation graphique
 evolution_tot <- as.data.frame(evolution_stations)
+evolution_tot$Date <- as.Date(paste0(evolution_tot$Var1, "-01"))
 #visualisation graphique
-plot(evolution_tot, 
+plot(evolution_tot$Date,
+    evolution_tot$Freq,
+    type ="o",
     main="Evolution du nombre de stations mises en service par année/mois",
     xlab = "Temps (année/mois)",
     ylab= "Nombre de station",
-    las =2)
+    ylim = c(0, 3000),
+    las =1)
+
+#On rentre toutes les années
+annees_exactes <- as.Date(c("2021-01-01", "2022-01-01", "2023-01-01", 
+                            "2024-01-01", "2025-01-01", "2026-01-01", "2027-01-01"))
+#On force l'axe des X à mettre toutes les années
+axis(1, 
+     at = annees_exactes, 
+     labels = c("2021", "2022", "2023", "2024", "2025", "2026", "2027"), 
+     las = 1)
+     
+#tri des données étape 2
+
+#mise en format et bornes
+data_analyse$categorie_puissance <- cut(data_analyse$puissance_nominale,
+                                    breaks = c(0, 3.7, 7.4, 11, 22, 43, 50, 150, Inf),
+                                    labels = c("3.7kW", "7.4kW", "11kW", "22kW", "43kW",
+                                     "50kW", "Fast(50-150)", "Ultra(>150)"))
+repartition_puissance <- table(data_analyse$categorie_puissance)
+barplot(repartition_puissance,
+    horiz = TRUE,
+    las = 1,
+    main ="Répartition des puissances en kw",
+    xlab = "Nombre de stations",
+    xlim =c(0,30000),
+    ylab = "Puissance")
